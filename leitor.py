525d9bd41cb59d16a6e53c87269d8825b29cf918
Pedro Bufulin <pedrohba18@gmail.com>
2020-11-30 21:06:08 -0300

    analisador léxico pronto

--- leitor.py
+++ leitor.py
@@ -10,31 +10,32 @@ class Token(NamedTuple):
     column: int
 
 def tokenize(code):
-    keywords = {'programa','se', 'entao', 'senao', 'enquanto', 'faca', 'para', 'int', 'real', 'char', 'or', 'and', 'not'}
+    keywords = {'programa','se', 'entao', 'senao', 'enquanto', 'faca', 'para', 'int', 'real', 'char', 'OR', 'AND', 'NOT'}
     token_specification = [
         ('ConstReal', r'\d(\d)*\.\d(\d)*'),   
         ('ConstInt', r'\d(\d)*'), 
         ('ConstChar', r'\'[\w+]?\''),
-        ('opAtribuicao',   r':='),           # Assignment operator
-        ('PVirg',      r';'),            # Statement terminator
-        ('opAdicao', r'\+'),                     # +
-        ('opSubtracao', r'-'),                     # -
-        ('opMult', r'\*'),                     # *
-        ('opDiv', r'\/'),                      # /
-        ('AbreParentese', r'\('),            # (
-        ('FechaParentese', r'\)'),           # )
-        ('AbreChave', r'\{'),                # {
-        ('FechaChave', r'\}'),               # }
-        ('LE', r'<='),              # <=
-        ('GE', r'>='),              # >=
-        ('GT', r'>'),               
-        ('LT', r'<'),
-        ('NE', r'!='),              # !=
-        ('EQ', r'=='),               # ==
-        ('ID',       r'(?<!\w)[a-zA-Z]\w*'),    # Identifiers
-        ('NEWLINE',  r'\n'),           # Line endings
-        ('SKIP',     r'[ \t]+'),       # Skip over spaces and tabs
-        ('MISMATCH', r'.'),            # Any other character
+        ('opAtribuicao',   r':='),          
+         ('virg',      r','),           
+        ('PVirg',      r';'),           
+        ('opAdicao', r'\+'),                    
+        ('opSubtracao', r'-'),                   
+        ('opMult', r'\*'),                    
+        ('opDiv', r'\/'),                     
+        ('AbreParentese', r'\('),          
+        ('FechaParentese', r'\)'),          
+        ('AbreChave', r'\{'),                
+        ('FechaChave', r'\}'),              
+        ('opMenorIgual', r'<='),              
+        ('opMaiorIgual', r'>='),   
+        ('opMaior', r'>'),
+        ('opMenor', r'<'),
+        ('opDiferente', r'<>'),             
+        ('opIgual', r'=='),             
+        ('ID',       r'(?<!\w)[a-zA-Z]\w*'),    
+        ('NEWLINE',  r'\n'),          
+        ('SKIP',     r'[ \t]+'),       
+        ('MISMATCH', r'.'),           
     ]
     tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
     line_num = 1
@@ -44,8 +45,18 @@ def tokenize(code):
         value = mo.group()
         column = mo.start() - line_start
         if tipo == 'ID' and value in keywords:
-            if value == 'and' or value == 'or' or value == 'not':
-                tipo = 'OpLogic'  
+            if value == 'AND':
+                tipo = 'opAnd'
+            elif value == 'OR':
+                tipo = 'opOr'
+            elif value == 'NOT':
+                        tipo = 'opNot'  
+            elif value == 'int':
+                        tipo = 'tipoInt'       
+            elif value == 'real':
+                        tipo = 'tipoReal'
+            elif value == 'char':
+                        tipo = 'tipoChar'
             else:
                 tipo = value.upper()
         elif tipo == 'NEWLINE':
