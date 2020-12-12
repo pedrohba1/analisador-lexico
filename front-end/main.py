a97345b69abc21dec38b780543b2286bee68f63d
Pedro Bufulin <pedrohba18@gmail.com>
2020-12-12 11:50:56 -0300

    parte do parser

--- front-end/main.py
+++ front-end/main.py
@@ -1,6 +1,9 @@
 from lexer import Lexer
-
+from parser import Parser
 lex =  Lexer('codigo.estudol')
 
 for token in lex.tokens:
-    print(token[0])
+    print(token)
+
+parser = Parser(lex.tokens)
+parser.parse()
--- front-end/parser.py
+++ front-end/parser.py
@@ -9,6 +9,15 @@ class Parser:
         if self.token_idx < len(self.tokens):
             self.current_token = self.tokens[self.token_idx]
 
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+
+    
+
+
 
 
 
